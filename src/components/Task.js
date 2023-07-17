fecdc41781c2647d45209e2164496f6f5b605cbd
AnasBnth10 <anasbnth@gmail.com>
2023-07-17 20:32:51 +0200

    Changed layout on Task Card

--- src/components/Task.js
+++ src/components/Task.js
@@ -29,12 +29,15 @@ function Task(props) {
                             class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{props.task.done
                                 ? "Completed"
                                 : "Not done yet"}</label>
+
+                        
+                                
                         <button
                             onClick={() => {
                             dispatch(deleteTask(props.task.id))
                         }}
                             type="button"
-                            class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                            class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-small rounded-full text-sm px-3 py-2.5 mx-2 text-center  dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
                     </div>
 
                 </div>
